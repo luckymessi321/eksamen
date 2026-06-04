200b003ae8eeec4a6dd5deb37ec2a9f9a24f1465
luckymessi321 <luckymessi321@gmail.com>
2026-06-04 07:28:00 +0000

    la til en funksjon "get_conn()" som oppretter en tilkobling mellom app.py og MySQL databasen på lokal server

--- lukas-eksamen/app.py
+++ lukas-eksamen/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session, request
+from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
+from forms import RegisterForm
 
 app = Flask(__name__)
+# importert fra presentasjon
+app.secret_key = "hemmelig-nok"
+
+# funksjon som oppretter en tilkobling til databasen 'login' med brukeren 'lukas'
+def get_conn():
+    return mysql.connector.connect(
+        host="localhost",
+        user="lukas",
+        password="messi123",
+        database="login"
+    )
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
