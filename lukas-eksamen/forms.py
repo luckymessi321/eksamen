1e940fdef6c91c905a24f2f705fdfbd6181e9ddf
luckymessi321 <luckymessi321@gmail.com>
2026-06-04 07:21:53 +0000

    opprettet en ny fil "forms.py"

---
+++ lukas-eksamen/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Register")
