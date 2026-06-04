7f18cf26feccde2342a2c6f085edbb736802a901
luckymessi321 <luckymessi321@gmail.com>
2026-06-04 08:11:12 +0000

    oppdaterte navigasjonsbar i base.html og imports i app.py

--- lukas-eksamen/app.py
+++ lukas-eksamen/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, session, request
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
-from forms import RegisterForm
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 # importert fra presentasjon
